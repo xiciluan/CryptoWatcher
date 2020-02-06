88c605283aa82bbd16155a54d37f6bb2152c5ad3
Xici Luan <xiciluan@gmail.com>
2020-02-06 15:33:30 -0800

    add comments and make modifications

--- SparkStreaming/get_monitor_data.py
+++ SparkStreaming/get_monitor_data.py
@@ -16,6 +16,11 @@ CHECKPOINT_PATH = '/dev/shm/spark'
 
 
 def get_gini_index(sorted_list):
+    '''
+    Calculate gini index within the time window.
+    - sorted_list: Ascending sorted list of total rewards given to different miners.
+    return: Gini index.
+    '''
     n = len(sorted_list)
     height = sum(val for val in sorted_list)
     area = sum((i+1)*val for i, val in enumerate(sorted_list))
@@ -23,10 +28,19 @@ def get_gini_index(sorted_list):
 
 
 def get_max_hash_rate(sorted_list):
+    '''
+    Calculate max_hash_rate within the time window.
+    - sorted_list: Ascending sorted list of total rewards given to different miners.
+    return: Max hash rate.
+    '''
     return round(sorted_list[-1]/sum(sorted_list), 2)
 
 
 def write_to_mysql(result):
+    '''
+    - result: Tuple of result to be written to database. (time: int, gini_index: float, max_hash_rate: float)
+    return: None. Results will be written to database.
+    '''
     mydb = mysql.connector.connect(
         host=os.environ["MYSQL_HOST"],
         port=3306,
@@ -43,6 +57,13 @@ def write_to_mysql(result):
 
 
 def handler(message):
+    '''
+    Define how to handle each RDD. 
+    Specifically, first collect them and get sorted lists of values of total rewards given to 
+    different miners. Then calculate decentralization indexs and write the result to database.
+    - message: RDD of tuples (miner, sum_rewards) within the time window. 
+    return None. Results will be written to database. 
+    '''
     orig = message.collect()
     records = defaultdict(float)
     if len(orig) == 0:
@@ -59,6 +80,10 @@ def handler(message):
 
 
 def createContext():
+    '''
+    Create streaming Context.
+    return streaming Context
+    '''
     sc = SparkContext(appName="GetMonitorData")
     ssc = StreamingContext(sc, 20)
 
@@ -68,7 +93,7 @@ def createContext():
     groups = lines\
         .map(lambda x: json.loads(x))\
         .map(lambda x: (str(x['miner']), x['rewards']))\
-        .reduceByKeyAndWindow(lambda x, y: x+y, lambda x, y: x-y, 60, 20)
+        .reduceByKeyAndWindow(lambda x, y: x+y, lambda x, y: x-y, windowSize, slideSize)
 
     # groups.pprint()
     groups.foreachRDD(handler)
@@ -78,7 +103,15 @@ def createContext():
 
 
 if __name__ == '__main__':
-    zkQuorum, topic = sys.argv[1:]
+    if len(sys.argv) != 5 and len(sys.argv) != 4:
+        msg = ("Usage: get_monitor_data.py <hostname> <port> "
+               "<window duration in seconds> [<slide duration in seconds>]")
+        print(msg, file=sys.stderr)
+        sys.exit(-1)
+    zkQuorum, topic = sys.argv[1], sys.argv[2]
+    windowSize = int(sys.argv[3])
+    slideSize = int(sys.argv[4]) if (len(sys.argv) == 5) else windowSize
+
     context = StreamingContext.getOrCreate(CHECKPOINT_PATH, createContext)
     context.start()
     context.awaitTermination()
